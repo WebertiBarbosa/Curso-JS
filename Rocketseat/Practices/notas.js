a55ea6dd9efe0cc2f65bf56241365fdeb83efe1f
weberticf@gmail.com <weberticf@gmail.com>
2021-03-05 19:37:24 -0300

    treinando

---
+++ Rocketseat/Practices/notas.js
@@ -0,0 +1,13 @@
+let notas = 90
+
+if (notas >= 90){
+  console.log(notas = "A")
+} else if (notas >= 80 && notas <= 89) {
+  console.log(notas = "B")
+} else if (notas >= 70 && notas <= 79){
+  console.log(notas = "C")
+} else if (notas >= 60 && notas <= 69){
+  console.log(notas = "D")
+} else {
+  console.log(notas = "F")
+}
